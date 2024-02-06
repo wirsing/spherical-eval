71c1faa1a1b86010b08a79cc12c45aa7f501ee84
Wilm Thoben <wirsing@Wilms-MacBook-Air.local>
2024-02-05 19:58:46 -0800

    Add percentile and jeni tier to data display.

--- components/DataDisplay.vue
+++ components/DataDisplay.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
     <h2> {{ data?.zip }}</h2>
+    <div>
+      <h3> Jeni Percentile {{ parseInt(data?.jenipctl || 0) }} %</h3>
+      <UMeter :value="parseInt(data?.jenipctl || 0)"></UMeter>
+    </div>
+    <div>
+      <h3> Jeni Tier </h3>
+      <h3> {{ data?.jenicategory }} </h3>
+    </div>
   </div>
 </template>
 
@@ -9,6 +17,7 @@ import { useMapStore } from '@/stores/mapStore';
 
 const mapStore = useMapStore();
 const { jeniData: data} = storeToRefs(mapStore)
+console.log(data?.jenipctl)
 </script>
 
 <style lang="scss">
